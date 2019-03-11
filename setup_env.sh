3ecd121c573e6dc72bd03c1d97b463200b02f14d
Dan Petrisko <petrisko@cs.washington.edu>
2019-03-11 10:47:26 +0000

    Minor hack

--- setup_env.sh
+++ setup_env.sh
@@ -39,7 +39,7 @@ fi
 
 # HACK
 rm -rf bsg_ip_cores
-git submodule update --init bsg_ip_cores --depth=50
+git submodule update --init bsg_ip_cores --depth 50
 
 if [ "$1" = "roms" ]; then
   # Make test roms
